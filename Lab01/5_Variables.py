2b8638e1ccd6e719191d4baabcce507d87ee6f5b
AbildayevTamerlan <tamerlanabildayev@gmail.com>
2025-01-16 20:53:46 +0500

    Закончил Lab 01

--- Lab01/5_Variables.py
+++ Lab01/5_Variables.py
@@ -1,16 +1,59 @@
-x = 5
-y = "John"
-print(x)
-print(y)
-
-
 x = 4       # x is of type int
 x = "Sally" # x is now of type str
-print(x)
 
 
-x = str(3)    # x will be '3'
-y = int(3)    # y will be 3
-z = float(3)  # z will be 3.0
+a = 4
+A = "Sally"
+#A will not overwrite a
 
 
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+x, y, z = "Orange", "Banana", "Cherry"
+
+
+x = y = z = "Orange"
+
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+
+
+x = 5
+y = 10
+print(x + y)
+
+
+x = 5
+y = "John"
+print(x, y)
+
+
+#-------------------------
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+
+#-------------------------
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
