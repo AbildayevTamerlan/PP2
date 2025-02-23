8a6857586968e7b9ce19f76e664cc45d005c330e
AbildayevTamerlan <tamerlanabildayev@gmail.com>
2025-02-23 23:13:55 +0500

    Lab 05

--- Lab05/regex.py
+++ Lab05/regex.py
@@ -1,6 +1,6 @@
 import re
 
-text_to_match = """
+text_to_match = """\
 John's email is john.doe@example.com, and his backup is johndoe123@work.net.  
 Call him at +1-555-123-4567 or his office line (555) 765-4321.  
 His website is https://www.johndoe.dev, and his IP address is 192.168.0.1.  
@@ -9,6 +9,8 @@ Today's date is 17/02/2025, and another format is 2025-02-17.
 Here's some HTML: <div class="container">Hello, World!</div>  
 Watch out for special characters like !@#$%^&*()_+={}[]|:;"'<>,.?/
 abbb
+
+NewLineOfText
 
 A regular expression (or RE) specifies a set of strings that matches it; 
 the functions in this module let you check if a particular string matches a given regular expression (or if a given regular expression matches a particular string, which comes down to the same thing). 
@@ -61,8 +63,54 @@ print(result, "\n")
 
 
 # Task 5
-pattern = r"a.+b"
+pattern = r"a.+?b"
 
 result = re.findall(pattern, text_to_match)
 
 print(result, "\n")
+
+
+# Task 6
+pattern = r"[\s,\.]"
+
+result = re.sub(pattern, ':', text_to_match)
+
+print(result, "\n")
+
+
+# Task 7
+pattern = r"_(\w)"
+
+def to_camel_case(match):
+   return match.group(1).upper()
+
+result = re.sub(pattern, to_camel_case, text_to_match)
+
+print(result)
+
+
+# Task 8
+pattern = r"[A-Z]"
+
+result = re.split(pattern, text_to_match)
+
+print(result, "\n")
+
+
+# Task 9
+pattern = r"([a-z])([A-Z])"
+
+result = re.sub(pattern, r"\1 \2", text_to_match)
+
+print(result)
+
+
+# Task 10
+pattern = r"([a-z])([A-Z])"
+
+def to_snake_case(match):
+   return f"{match.group(1)}_{match.group(2).lower()}"
+
+result = re.sub(pattern, to_snake_case, text_to_match)
+
+print(result)
